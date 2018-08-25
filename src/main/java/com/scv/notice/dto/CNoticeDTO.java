310fda26f9601b716436f05fcab55340aa0e570b
Leejinhyo <41771214+Leejinhyo@users.noreply.github.com>
2018-08-25 12:41:35 +0900

    photoBoardDTO 및 CNoticeDTO

--- src/main/java/com/scv/notice/dto/CNoticeDTO.java
+++ src/main/java/com/scv/notice/dto/CNoticeDTO.java
@@ -1,6 +1,11 @@
 package com.scv.notice.dto;
 
+/**
+ * @author jinhy
+ *	전체공지사항 DTO
+ */
 public class CNoticeDTO
 {
-
+	// 전체공지사항코드
+	int sNoticeCode;
 }
--- src/main/java/com/scv/photoboard/dto/PhotoBoardDTO.java
+++ src/main/java/com/scv/photoboard/dto/PhotoBoardDTO.java
@@ -1,6 +1,46 @@
 package com.scv.photoboard.dto;
 
+/**
+ * @author jinhy
+ *	활동사진 게시판
+ */
 public class PhotoBoardDTO
 {
-
+	// 활동사진게시판코드
+	int photoBoardCode;
+	String pBSubject, pBContent, pBRegDate;
+	
+	
+	public int getPhotoBoardCode()
+	{
+		return photoBoardCode;
+	}
+	public void setPhotoBoardCode(int photoBoardCode)
+	{
+		this.photoBoardCode = photoBoardCode;
+	}
+	public String getpBSubject()
+	{
+		return pBSubject;
+	}
+	public void setpBSubject(String pBSubject)
+	{
+		this.pBSubject = pBSubject;
+	}
+	public String getpBContent()
+	{
+		return pBContent;
+	}
+	public void setpBContent(String pBContent)
+	{
+		this.pBContent = pBContent;
+	}
+	public String getpBRegDate()
+	{
+		return pBRegDate;
+	}
+	public void setpBRegDate(String pBRegDate)
+	{
+		this.pBRegDate = pBRegDate;
+	}
 }
