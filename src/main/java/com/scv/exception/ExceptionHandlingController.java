7ac966b8c09f694b559a8e5987e613542a51d881
admin-PC\Administrator <qhrms7867@gmail.com>
2018-08-27 17:50:15 +0900

    진행 (예외처리)

---
+++ src/main/java/com/scv/exception/ExceptionHandlingController.java
@@ -0,0 +1,22 @@
+package com.scv.exception;
+
+import java.sql.SQLException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ExceptionHandlingController{
+
+	private static final Logger logger = LoggerFactory.getLogger(ExceptionHandl)
+
+}
