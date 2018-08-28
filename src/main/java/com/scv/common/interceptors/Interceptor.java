4fae99daa721415306616c1bbe3bc9fe01c885d6
admin-PC\Administrator <qhrms7867@gmail.com>
2018-08-28 13:32:22 +0900

    관리자) 로그인 처리 완벽 끝 + 스프링 버전업

--- src/main/java/com/scv/common/interceptors/Interceptor.java
+++ src/main/java/com/scv/common/interceptors/Interceptor.java
@@ -14,39 +14,22 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
  * 작성 일자 : 2018. 8. 26. 오후 4:31:25
  */
 public class Interceptor extends HandlerInterceptorAdapter{
-	private static final Logger logger = LoggerFactory.getLogger(Interceptor.class);
-	
-	static final String[] EXCLUDE_URL_LIST = {
-			"/login"
-	};
 
 	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-			throws Exception {
-		String reqUrl = request.getRequestURL().toString();    
-//		System.out.println("인터셉터 요청 url : " + reqUrl);
-		if (reqUrl.equals("/loginCheck.do")) {
-			return true;
-		}
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler){
+		try {
+				//logininfo 세션값이 널일경우 
+				if(request.getSession().getAttribute("adminCode") == null ){ 
+					//로그인페이지로 redirect
+					response.sendRedirect("login.do");
+				
+					return false;
+				}
+			} catch (Exception e) { 
+				e.printStackTrace(); 
+			} //널이 아니면 정상적으로 컨트롤러 호출 
 		
-		for (String target : EXCLUDE_URL_LIST) {
-			if (reqUrl.indexOf(target) > -1) {
-				return true;
-			}
-		}
-
-		HttpSession session = request.getSession();
-		int adminCode = Integer.parseInt(toString().valueOf(session.getAttribute("adminCode")));
-		
-		if (adminCode == 0) {
-			logger.info(">> interceptor catch!!! userId is null.. ");
-			session.invalidate();
-			response.sendRedirect(request.getContextPath() + "/login.do");
-
-			return false;
-		}
-
-		return true;
+		return true; 
 	}
 }
 
--- src/main/java/com/scv/exception/ExceptionHandlingController.java
+++ src/main/java/com/scv/exception/ExceptionHandlingController.java
@@ -10,13 +10,14 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@ControllerAdvice
 public class ExceptionHandlingController{
 
-	private static final Logger logger = LoggerFactory.getLogger(ExceptionHandl)
+	private static final Logger logger = LoggerFactory.getLogger(ExceptionHandlingController.class);
 
 }
